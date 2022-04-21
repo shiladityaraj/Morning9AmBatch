a85560d897cc32ebbbdea251e38fee836496ecde
SakshamDigitalTechnology <info@sakshamdigitaltechnology.com>
2022-04-21 09:35:44 +0530

    radiobutton

--- app/src/main/java/s/d/morningbatchproject/ThirdActivity.java
+++ app/src/main/java/s/d/morningbatchproject/ThirdActivity.java
@@ -10,5 +10,6 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
     }
 }
