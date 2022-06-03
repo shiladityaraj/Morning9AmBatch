b4fb3c67f9acbe362680df222772e24dfb923a4a
SakshamDigitalTechnology <info@sakshamdigitaltechnology.com>
2022-06-03 10:58:01 +0530

    google map(3/6/2022)

--- app/src/main/java/s/d/morningbatchproject/googleMap/MapsActivity.java
+++ app/src/main/java/s/d/morningbatchproject/googleMap/MapsActivity.java
@@ -1,11 +1,18 @@
 package s.d.morningbatchproject.googleMap;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,16 +20,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.List;
 
 import s.d.morningbatchproject.R;
 import s.d.morningbatchproject.databinding.ActivityMapsBinding;
 
 
 public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback {
+        implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +43,27 @@ public class MapsActivity extends FragmentActivity
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 0, 0, this);
+
     }
 
     @Override
@@ -57,16 +86,72 @@ public class MapsActivity extends FragmentActivity
         LatLng l1=new
                 LatLng(23.2526,
                 77.4652);
+
+        LatLng l2=new
+                LatLng(23.2345,
+                77.45678);
+
+        LatLng l3=new
+                LatLng(23.1264,
+                77.1652);
+
+        LatLng l4=new
+                LatLng(23.11226,
+                77.6543);
         mMap.addMarker(new MarkerOptions()
                 .position(l1)
                 .title("Saksham digital technology"));
+        mMap.addMarker(new MarkerOptions()
+                .position(l2)
+                .title("Saksham digital technology"));
+        mMap.addMarker(new MarkerOptions()
+                .position(l3)
+                .title("Saksham digital technology"));
+        mMap.addMarker(new MarkerOptions()
+                .position(l4)
+                .title("Saksham digital technology"));
 
+        mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(23.2526, 77.4652))
+                .add(new LatLng(23.2345,
+                        77.45678))
+                .add(new LatLng(23.1264,
+                        77.1652))
+                .add(new LatLng(23.11226,
+                        77.6543))
+                .add(new LatLng(23.2526, 77.4652)).width(10));
+    }
 
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double lat=location.getLatitude();
+        double lng=location.getLongitude();
+        Log.e("MyLocation",lat+"== "+lng);
 
+    }
 
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
     }
 }
